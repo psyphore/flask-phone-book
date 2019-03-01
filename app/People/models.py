d44edc40fe515e67147e0f1b7154c3e0232ac22e
Sipho Hlophe <sipho@iplatform.co.za>
2019-03-01 14:28:28 +0200

    refactor: move out person actions into a service

--- app/People/models.py
+++ app/People/models.py
@@ -2,9 +2,10 @@ from py2neo.ogm import Property, RelatedTo, Label
 
 from app.models import BaseModel
 
+
 class Person(BaseModel):
     __primarykey__ = 'email'
-    
+
     person = Label("Person")
 
     title = Property()
@@ -14,6 +15,18 @@ class Person(BaseModel):
     email_address = Property('emailAddress')
     date_updated = Property('updatedOn')
 
-    receipts = RelatedTo('Receipt', 'HAS')
-    stores = RelatedTo('Store', 'GOES_TO')
+    # receipts = RelatedTo('Receipt', 'HAS')
+    # stores = RelatedTo('Store', 'GOES_TO')
 
+    def as_dict(self):
+        return {
+            'title': self.title,
+            'firstname': self.firstname,
+            'lastname': self.lastname,
+            'mobileNumber': self.mobile_number,
+            'email': self.email_address,
+            'lastUpdatedOn': self.date_updated
+        }
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
--- app/People/schema.py
+++ app/People/schema.py
@@ -1,3 +1,69 @@
 import graphene
 
 from .models import Person
+from .service import PeopleService
+
+
+class PersonSchema(graphene.ObjectType):
+    title = graphene.String()
+    firstname = graphene.String()
+    lastname = graphene.String()
+    mobile_number = graphene.String()
+    email_address = graphene.String()
+    date_updated = graphene.DateTime()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        svc = PeopleService()
+        self.customer = svc(email=self.email).fetch()
+
+    def resolve_team(self, info):
+        # return [StoreSchema(**store.as_dict()) for store in self.customer.stores]
+        pass
+
+    def resolve_manager(self, info):
+        # return [ReceiptSchema(**receipt.as_dict()) for receipt in self.customer.receipts]
+        pass
+
+    def resolve_products(self, info):
+        # return [ProductSchema(**product.as_dict()) for product in self.customer.products]
+        pass
+
+
+class CreatePerson(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    success = graphene.Boolean()
+    customer = graphene.Field(lambda: CustomerSchema)
+
+    def mutate(self, info, **kwargs):
+        customer = Customer(**kwargs)
+        customer.save()
+
+        return CreateCustomer(customer=customer, success=True)
+
+
+class Query(graphene.ObjectType):
+    customer = graphene.Field(lambda: CustomerSchema, email=graphene.String())
+    stores = graphene.List(lambda: StoreSchema)
+    products = graphene.List(lambda: ProductSchema)
+
+    def resolve_customer(self, info, email):
+        customer = Customer(email=email).fetch()
+        return CustomerSchema(**customer.as_dict())
+
+    def resolve_stores(self, info):
+        return [StoreSchema(**store.as_dict()) for store in Store().all]
+
+    def resolve_products(self, info):
+        return [ProductSchema(**product.as_dict()) for product in Product().all]
+
+
+class Mutations(graphene.ObjectType):
+    create_customer = CreateCustomer.Field()
+    submit_receipt = SubmitReceipt.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutations, auto_camelcase=True)
