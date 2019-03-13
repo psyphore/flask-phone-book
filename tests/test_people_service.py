5ff9649838c97b0f851eac01a25da0a078f48f56
sipho hlophe <psyphore@gmail.com>
2019-03-13 08:37:31 +0200

    test: add unit test - test more person service functions - set up vscode debugger configuration

--- tests/test_people_service.py
+++ tests/test_people_service.py
@@ -5,8 +5,20 @@ service = PeopleService()
 
 
 class TestPeopleService(unittest.TestCase):
-    def test_search(self):
+    def test_fetch_all(self):
         self.assertIsNotNone(service)
-        query, limit = 'Sipho', 5
-        search_value = service.filter(query=query, limit=limit)
-        self.assertTrue(len(search_value) > 0)
+        limit = 2
+        search_value = service.fetch_all(limit=limit)
+        self.assertTrue(len(search_value) > 0 and len(search_value) <= limit)
+
+    def test_fetch_one(self):
+        self.assertIsNotNone(service)
+        _id = '6cdbc440-9056-4470-9f98-e30cf43ab0f0'
+        item_value = service.fetch(_id)
+        self.assertIsNotNone(item_value)
+
+    def test_fetch_team(self):
+        _id = '6cdbc440-9056-4470-9f98-e30cf43ab0f0'
+        item_value = service.fetch(_id)
+        self.assertIsNotNone(item_value)
+        
