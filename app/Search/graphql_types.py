6a586ad15212b80f5d693c788edca09b032e5c20
sipho hlophe <psyphore@gmail.com>
2019-03-16 17:03:55 +0200

    refactor: wireup advanced search with input object type

--- app/Search/graphql_types.py
+++ app/Search/graphql_types.py
@@ -9,15 +9,17 @@ class SearchType(graphene.InputObjectType):
         :offest, number to offset by a set
     '''
     query = graphene.NonNull(graphene.String)
-    first = graphene.Int(10)
-    offset = graphene.Int(0)
-    # class Arguments:
-    #     query = graphene.String(required=True)
-    #     first = graphene.Int(10)
-    #     offset = graphene.Int(0)
+    first = graphene.Int()
+    offset = graphene.Int()
+
+    @property
+    def criteria(self):
+        return f"({self.query},{self.first},{self.offset})"
 
 class SearchResultType(graphene.ObjectType):
-    '''Search Result, containing a count of items contained in the items member'''
+    '''Search Result, 
+        containing a count of items contained in the data member
+    '''
 
     count = graphene.Int(0)
     data = graphene.List(lambda: PersonType)
