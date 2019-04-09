68324e8d80163d42a433923e1988722f8d2e5135
Sipho Hlophe <sipho@iplatform.co.za>
2019-04-09 11:44:36 +0200

    refactor: use a global search service instead of using the service from init

--- app/Search/schema.py
+++ app/Search/schema.py
@@ -6,7 +6,7 @@ from .service import SearchService
 from app.People.graphql_types import  PersonType
 from .graphql_types import SearchType, SearchResultType
 
-# service = SearchService()
+service = SearchService()
 
 class SearchQuery(graphene.ObjectType):   
     '''Search Query, 
@@ -15,13 +15,10 @@ class SearchQuery(graphene.ObjectType):
 
     search = graphene.Field(SearchResultType, query=graphene.NonNull(graphene.String), limit=graphene.Int(10))
     search_advanced = graphene.Field(SearchResultType, criteria=graphene.NonNull(SearchType))
-    
-    def __init__(self):
-        self.service = SearchService()
-
+        
     def resolve_search(self, info, **args):
         q, l = args.get("query"), args.get("limit")
-        result = self.service.filter(query=q,limit=l)
+        result = service.filter(query=q,limit=l)
 
         if result is None:
             raise GraphQLError(f'"{q}" has not been found in our people list.')
@@ -33,7 +30,7 @@ class SearchQuery(graphene.ObjectType):
         return sr
 
     def resolve_search_advanced(self, info, criteria):
-        result = self.service.filter(query=criteria.query,limit=criteria.first,skip=criteria.offset)
+        result = service.filter(query=criteria.query,limit=criteria.first,skip=criteria.offset)
 
         if result is None:
             raise GraphQLError(f'"{criteria.query}" has not been found in our people list.')
